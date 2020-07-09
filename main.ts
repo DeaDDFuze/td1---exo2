1c70d812dca639a10b75a8e82e468e214b91f7ea
DeaDDFuze <59582709+DeaDDFuze@users.noreply.github.com>
2020-07-09 14:03:35 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ for (let index = 0; index < 2; index++) {
     basic.pause(500)
     basic.showString("10853")
     basic.pause(500)
-    basic.showIcon(IconNames.Sword)
+    basic.showArrow(ArrowNames.South)
     basic.pause(500)
     basic.showIcon(IconNames.Happy)
     basic.pause(500)
